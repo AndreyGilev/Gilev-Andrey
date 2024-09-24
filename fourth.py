e6d067ae8612db8db0c1fde72e2a919da01ecafe
Гилёв Андрей <andreygil0808@gmail.com>
2024-09-24 12:13:24 +0300

    c

--- fourth.py
+++ fourth.py
@@ -1,26 +1,20 @@
 import RPi.GPIO as gpio
-import time
 gpio.setmode(gpio.BCM)
-dac = [6, 12, 5, 0, 1, 7, 11, 8]
-gpio.setup(dac, gpio.OUT)
+gpio_out = 24
+gpio.setup(gpio_out, gpio.OUT)
+a = [2, 3, 4, 17, 27, 22, 10, 9]
+gpio.setup(a, gpio.OUT)
+gpio.output(a, 0)
 
-def tran(n):
-    n2 = [0] * 8
-    for i in range(8):
-        n2[i] = n%2
-        n//=2
-    return n2
-period = float(input())
-
+p = gpio.PWM(gpio_out, 10000)
+n = 0
 try:
+    
     while True:
-        for n in range(256):
-            n2 = tran(n)
-            gpio.output(dac, n2)
-            time.sleep(period/512)
-        for n in range(255, -1, -1):
-            n2 = tran(n)
-            gpio.output(dac, n2)
-            time.sleep(period/512)
+        p.start(n)
+        n = int(input())
+        
 finally:
-    gpio.output(dac, 0)
+    p.stop()
+    gpio.output(gpio_out, 0)
+
