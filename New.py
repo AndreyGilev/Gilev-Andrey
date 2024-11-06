9b9325d4fa6482d9bfd598e91addf0ffb474d1cd
AndreyGilev <andreygil0808@gmail.com>
2024-11-06 19:42:19 +0300

    Update New.py
    Добавил комментарии

--- New.py
+++ New.py
@@ -9,62 +9,64 @@ gpio.setmode(gpio.BCM)
 dac = [8, 11, 7, 1, 0, 5, 12, 6]
 
 gpio.setup(dac, gpio.OUT)
-gpio.setup(troyka, gpio.OUT)
+gpio.setup(troyka, gpio.OUT)                    # Настраиваем Gpio
 gpio.setup(comp, gpio.IN)
+
 
 def binary(n):
     a = [0] * 8
-    for i in range(8):
-        a[8-i-1] = n % 2   
-        n = n//2
+    for i in range(8):                          # Функция перевода числа в двоичную запись
+        a[8 - i - 1] = n % 2
+        n = n // 2
     return a
+
 
 def adc():
     k = 0
     for i in range(7, -1, -1):
-        k += 2**i 
-        gpio.output(dac, binary(k))
+        k += 2 ** i
+        gpio.output(dac, binary(k))             # Функция, выдающая цифровое значение подаваемого аналогого напряжения
         time.sleep(0.01)
         if gpio.input(comp) == 1:
-            k-=2**i
+            k -= 2 ** i
     return k
 
 
-
 try:
-    arr = np.array([])
-    k = 0
+    arrdigitalU = np.array([])                  # Объявление массива, хранящего значения напржения в цифровом виде
+    u = 0
     count = 0
-    t1 = time.time() 
+    t1 = time.time()
     gpio.output(troyka, 1)
 
-    while k <= 206:
-        count += 1
-        k = adc()
-        arr = np.append(arr, k)
-        print(k)
+    while u <= 206:                             # Запонение массива с напряжением при зарядке конденчатора
+        count += 1                              # Считаем, сколько отсчетов приходит
+        u = adc()
+        arrdigitalU = np.append(arrdigitalU, u)
+        print(u)
     gpio.output(troyka, 0)
 
-    while k >= 0.02 * 256:
+    while u >= 0.02 * 256:                      # Запонение массива с напряжением при разрядке конденчатора
         count += 1
-        k = adc()
-        arr = np.append(arr, int(k))
-        print(k)
+        u = adc()
+        arrdigitalU = np.append(arrdigitalU, int(u))
+        print(u)
 
     t2 = time.time()
-    a = np.array([])
+    counter = np.array([])
 
-    for i in range(count):
-        a = np.append(a, i)
-    with open('data.txt', 'w') as data:
+    for i in range(count):                      # Заполнение массива числа отсчетов
+        counter = np.append(counter, i)
+    with open('data.txt', 'w') as data:         # Запись в текстовый файл цифровых значений напряжения
         for i in range(count):
-            data.write(str(int(arr[i])) + ' ')
-    with open('settings.txt', 'w') as set:
-        s = 'Частота дискретизации: '+ str(count/(t2-t1)) + "Шаг квантования: " + str(3.3/255)
+            data.write(str(int(arrdigitalU[i])) + ' ')
+    with open('settings.txt', 'w') as set:      # Запись в другой текстовый файл параметров системы
+        s = 'Частота дискретизации: ' + str(count / (t2 - t1)) + "Шаг квантования: " + str(3.3 / 255)
         set.write(s)
-        
-    print("Время: ", t2-t1, "Период одного измерения: ", (t2-t1)/count, "Частота дискретизации: ", count/(t2-t1), "Шаг квантования: ", 3.3/255)
-    plt.plot(a, arr)
+
+    print("Время: ", t2 - t1, "Период одного измерения: ", (t2 - t1) / count, "Частота дискретизации: ",
+          count / (t2 - t1), "Шаг квантования: ", 3.3 / 255)
+    plt.plot(counter, arrdigitalU)         # Построение графика значения цифрового значения напряжения от номера отсчета
     plt.show()
 finally:
     gpio.output(dac, 0)
